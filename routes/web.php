e4abd9dae5aa9878b206ccd9b052d6914ca8f022
unknown <you@example.com>
2026-06-03 17:40:04 -0500

    create de model y migrate

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('web.home');
 })->name('web.home');
-
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::view('/', 'admin.dashboard')->name('dashboard');
-});
